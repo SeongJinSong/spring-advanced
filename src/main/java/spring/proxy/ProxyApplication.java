a3b3044948762d0a09e9858a43c82bf738cbc5a9
송성진 <seongjin_song@tmax.co.kr>
2022-02-05 21:08:45 +0900

    proxy 프로젝트 생성

---
+++ src/main/java/spring/proxy/ProxyApplication.java
@@ -0,0 +1,13 @@
+package spring.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "spring.proxy.app") //주의
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+}
