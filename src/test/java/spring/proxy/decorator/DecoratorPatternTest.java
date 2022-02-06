ca710ec44c2376942cff58634c47c82f5a8b1910
seongjin_song <seongjin_song@tmax.co.kr>
2022-02-06 14:28:17 +0900

    데코레이터 패턴 - 예제 코드2

--- src/test/java/spring/proxy/decorator/DecoratorPatternTest.java
+++ src/test/java/spring/proxy/decorator/DecoratorPatternTest.java
@@ -2,7 +2,9 @@ package spring.proxy.decorator;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import spring.proxy.decorator.code.Component;
 import spring.proxy.decorator.code.DecoratorPatternClient;
+import spring.proxy.decorator.code.MessageDecorator;
 import spring.proxy.decorator.code.RealComponent;
 
 @Slf4j
@@ -11,6 +13,13 @@ public class DecoratorPatternTest {
     void noDecorator(){
         RealComponent realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+    @Test
+    void decorator1(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
