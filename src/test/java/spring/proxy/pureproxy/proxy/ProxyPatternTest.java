87d4ae34b178f228a6fe66b8735f5de3e66d319a
seongjin_song <seongjin_song@tmax.co.kr>
2022-02-06 14:15:59 +0900

    프록시 패턴 - 예제 코드2

--- src/test/java/spring/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/spring/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,6 +1,7 @@
 package spring.proxy.pureproxy.proxy;
 
 import org.junit.jupiter.api.Test;
+import spring.proxy.pureproxy.proxy.code.CacheProxy;
 import spring.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import spring.proxy.pureproxy.proxy.code.RealSubject;
 
@@ -9,6 +10,15 @@ public class ProxyPatternTest {
     void noProxyTest(){
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+    @Test
+    void cacheProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
