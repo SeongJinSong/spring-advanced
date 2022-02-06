7823ef67836247c8fcae8fd1133f984af52241f0
seongjin_song <seongjin_song@tmax.co.kr>
2022-02-06 21:35:49 +0900

    인터페이스 기반 프록시 - 적용

--- src/main/java/spring/proxy/ProxyApplication.java
+++ src/main/java/spring/proxy/ProxyApplication.java
@@ -2,11 +2,16 @@ package spring.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import spring.proxy.config.AppV1Config;
 import spring.proxy.config.AppV2Config;
+import spring.proxy.config.v1_proxy.InterfaceProxyConfig;
+import spring.proxy.trace.logtrace.LogTrace;
+import spring.proxy.trace.logtrace.ThreadLocalLogTrace;
 
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "spring.proxy.app") //주의
 public class ProxyApplication {
 
@@ -14,4 +19,8 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace(){
+		return new ThreadLocalLogTrace();
+	}
 }
--- src/main/java/spring/proxy/config/AppV1Config.java
+++ src/main/java/spring/proxy/config/AppV1Config.java
@@ -7,15 +7,15 @@ import spring.proxy.app.v1.*;
 @Configuration
 public class AppV1Config {
     @Bean
-    public OrderControllerV1 orderControllerV1(){
-        return new OrderControllerV1Impl(orderServiceV1());
-    }
+    public OrderControllerV1 orderControllerV1(){return new OrderControllerV1Impl(orderServiceV1());}
 
-    private OrderServiceV1 orderServiceV1() {
+    @Bean
+    public OrderServiceV1 orderServiceV1() {
         return new OrderServiceV1Impl(orderRepositoryV1());
     }
 
-    private OrderRepositoryV1 orderRepositoryV1() {
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1() {
         return new OrderRepositoryV1Impl();
     }
 }
