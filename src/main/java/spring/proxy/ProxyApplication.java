64618fdb330fa683f6728bf8a00dc25d601caa94
seongjin_song <seongjin_song@tmax.co.kr>
2022-02-07 21:04:00 +0900

    프록시 팩토리 - 적용2

--- src/main/java/spring/proxy/ProxyApplication.java
+++ src/main/java/spring/proxy/ProxyApplication.java
@@ -11,6 +11,7 @@ import spring.proxy.config.v1_proxy.InterfaceProxyConfig;
 import spring.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import spring.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import spring.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import spring.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import spring.proxy.trace.logtrace.LogTrace;
 import spring.proxy.trace.logtrace.ThreadLocalLogTrace;
 
@@ -19,7 +20,8 @@ import spring.proxy.trace.logtrace.ThreadLocalLogTrace;
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "spring.proxy.app") //주의
 public class ProxyApplication {
 
